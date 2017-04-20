d213a257b3fa7bc4f937aca51b14ef52eb7e32ef
likun <516947868@qq.com>
2017-04-20 11:34:49 +0800

    2013

--- tecnews.py
+++ tecnews.py
@@ -3,9 +3,9 @@ from bs4 import BeautifulSoup
 import simpletool
 import os
 
-year = 2013
-month = ['06', '07', '08', '09', '10', '11', '12']
-# month = ['11', '12']
+year = 2014
+month = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
+#month = ['07', '08', '09', '10', '11', '12']
 date = 31
 page_base = 'http://digitalpaper.stdaily.com/http_www.kjrb.com/kjrb/html/'
 count = 0
@@ -18,26 +18,29 @@ def tec_craw():
             content_id = x['href']
             if content_id[-2:] == str(-1):
                 count += 1
-                print('processed : ' + str(count) + ' article')
                 content_page = day_page + content_id
                 r_content = requests.get(content_page)
                 b_content = BeautifulSoup(r_content.content.decode('utf8'), 'html.parser')
                 with open(file_name, 'a', encoding='utf-8') as f:
+                    title = b_content.find("div", class_="biaoti")
                     if type(b_content.title.string) is not None:
-                        f.write('Title--'+b_content.title.string.strip()[:-8])
+                        if title.string is not None:
+                            f.write('Title--' + title.string + '\n')
+                        else:
+                            f.write('Title--' + '\n')
                         for p in b_content.find_all('p'):
                             if p.string is not None:
                                 sents = p.string.lstrip()
-                                sents_cut = simpletool.cut_sent(sents)
-                                for sent in sents_cut:
-                                    if len(sent) >= 8:
-                                        f.write(sent + '\n')
+                                f.write(sents + '\n')
+                        f.write('\n')
 
-    dir_name = 'C:/Users/51694/PycharmProjects/crawler/' + str(year)
-    if str(year) not in os.listdir('C:/Users/51694/PycharmProjects/crawler/'):
+
+    dir_name = r'C:\Users\Administrator\PycharmProjects\crawler' + '\\' + str(year)
+    if str(year) not in os.listdir(r'C:\Users\Administrator\PycharmProjects\crawler'):
         os.mkdir(dir_name)
     for m in month:
         for d in range(1, date+1):
+            print('processed: ' + str(year) + '.' + m + '.' + str(d) + ' articles')
             try:
                 if len(str(d)) < 2:
                     str_d = '0' + str(d)
@@ -63,9 +66,4 @@ def tec_craw():
                 print('-----------------'+m+d+'----------------------')
                 continue
 if __name__ == '__main__':
-
-
     tec_craw()
-    # r = requests.get('http://digitalpaper.stdaily.com/http_www.kjrb.com/kjrb/html/2015-02/10/node_2.htm')
-    # b = BeautifulSoup(r.content.decode('utf8'), 'html.parser')
-    # print(b('title')[0].string)
